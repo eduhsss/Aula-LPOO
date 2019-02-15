94b0088f73a3746a2f2d99e1a2637860085bbce7
Eduhss <47652040+Eduhss@users.noreply.github.com>
2019-02-15 02:15:12 -0200

    Add files via upload

---
+++ RevisaoObjetos/src/Pacote/NovaClasse.java
@@ -0,0 +1,20 @@
+
+package Pacote;
+
+
+public class NovaClasse {
+    public int a; //Atributo ♥
+    private int b;// Atributo ♥
+    
+    public void alomundo()
+    {
+        System.out.println("Brunette");           
+    }
+    public int calculo(){
+    int i=2;
+    int j=3;
+    return i+j;
+    
+    }
+    
+}
---
+++ RevisaoObjetos/src/revisaoobjetos/RevisaoObjetos.java
@@ -0,0 +1,15 @@
+
+package revisaoobjetos;
+
+import Pacote.NovaClasse;
+
+
+public class RevisaoObjetos {
+
+    public static void main(String[] args) {
+        NovaClasse nc = new NovaClasse();
+        System.out.println(nc.calculo());       
+        
+    }
+    
+}
